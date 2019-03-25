a1352806697fe1e73daa2096e4dba4945fd80eaf
Philipp Seitz <seitzphilipp22@gmail.com>
2019-03-25 10:00:34 +0100

    implemented the data Objects Machine and Report

---
+++ data_objects/Machine.py
@@ -0,0 +1,9 @@
+class Machine:
+
+    def __init__(self, machine_name, machine_data):
+        self.machine_name = machine_name
+        self.machine_data = machine_data
+
+    def get_machine_data(self):
+        return self.machine_data
+
---
+++ data_objects/Report.py
@@ -0,0 +1,7 @@
+class Report:
+
+    def __init__(self, first_machine, second_machine, calculated_similarity):
+        self.first_machine = first_machine
+        self.second_machine_name = second_machine
+        self.calculated_similarity = calculated_similarity
+
